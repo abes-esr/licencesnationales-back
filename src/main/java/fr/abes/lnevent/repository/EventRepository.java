9a4af2f11cd6d6788f9b29e03f9e5b343adf10c2
SimonLmrr <70648447+SimonLmrr@users.noreply.github.com>
2021-04-16 11:04:37 +0200

    Mise en place du service de report pour les editeurs

--- src/main/java/fr/abes/lnevent/repository/EventRepository.java
+++ src/main/java/fr/abes/lnevent/repository/EventRepository.java
@@ -4,6 +4,11 @@ import fr.abes.lnevent.entities.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+    List<EventEntity> getAllByDateCreationEventBetweenOrderByDateCreationEvent(Date dateDebut, Date dateFin);
 }
