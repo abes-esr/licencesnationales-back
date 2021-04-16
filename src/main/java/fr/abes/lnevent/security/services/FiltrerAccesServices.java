6d5692daebec6092b25735f49810258a09ec382c
charalambou <ecu@abes.fr>
2021-04-16 15:56:11 +0200

    choix à faire sur comparaison siren pour filtrer sur services

--- src/main/java/fr/abes/lnevent/security/services/FiltrerAccesServices.java
+++ src/main/java/fr/abes/lnevent/security/services/FiltrerAccesServices.java
@@ -30,19 +30,24 @@ public class FiltrerAccesServices {
             log.info("sirenFromController = " + sirenFromController);
             ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
             log.info("jwt = " + tokenProvider.getJwtFromRequest(attr.getRequest()));
-            String jwt = tokenProvider.getJwtFromRequest(attr.getRequest());
-            String siren = tokenProvider.getSirenFromJwtToken(jwt);
-            log.info("siren = " + siren);
+            String jwtContenuDansLaRequest = tokenProvider.getJwtFromRequest(attr.getRequest());
+            String sirenFromJwt = tokenProvider.getSirenFromJwtToken(jwtContenuDansLaRequest);
+            log.info("siren = " + sirenFromJwt);
             log.info("userDetails = " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
             UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             String sirenFromSecurityContextUser = userDetails.getUsername();
             log.info("sirenFromSecurityContextUser = " + sirenFromSecurityContextUser);
 
-            if(!siren.equals(sirenFromController)){
+            //le test d'acceptance :
+            //soit on compare sirenFromController avec sirenFromJwtContenuDansLaRequest
+            //soit on compare sirenFromController avec sirenFromSecurityContextUser => là on remonte un peu plus haut que le token
+
+
+            if(!sirenFromJwt.equals(sirenFromController)){
                 log.error("Acces interdit");
                 throw new AccesInterditException("Acces interdit");
             }
-            boolean existeSiren = etablissementRepository.existeSiren(siren);
+            boolean existeSiren = etablissementRepository.existeSiren(sirenFromJwt);
             log.info("existeSiren = "+ existeSiren);
             if (!existeSiren) {
                 log.error("Siren absent de la base");
