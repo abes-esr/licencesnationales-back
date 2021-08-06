9f066411b640970e09f76622cfcdd005ef8f6e3a
charalambou <ecu@abes.fr>
2021-08-06 20:06:45 +0200

    tests AuthenticationController : pour faire marcher le test, dans le MockUserUtil mettre "secret" au lieu de passwordEncoder.encode("OldPass1Test&")

--- web/src/main/java/fr/abes/licencesnationales/web/controllers/AuthenticationController.java
+++ web/src/main/java/fr/abes/licencesnationales/web/controllers/AuthenticationController.java
@@ -1,6 +1,8 @@
 package fr.abes.licencesnationales.web.controllers;
 
 
+import fr.abes.licencesnationales.core.exception.AccesInterditException;
+import fr.abes.licencesnationales.core.exception.SirenIntrouvableException;
 import fr.abes.licencesnationales.web.security.jwt.JwtTokenProvider;
 import fr.abes.licencesnationales.web.security.payload.request.LoginRequest;
 import fr.abes.licencesnationales.web.security.payload.response.JwtAuthenticationResponse;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,16 +38,16 @@ public class AuthenticationController {
     @ApiOperation(value = "permet de s'authentifier et de récupérer un token.",
             notes = "le token doit être utilisé pour accéder aux ressources protegées.")
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) throws NullPointerException { //wrapped in a NestedServletException
         var authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getLogin(), loginRequest.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
         //on charge dans user le tuple bdd mis dans authentication
-        UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
+        UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal(); //ici si mauvais credentials NestedServletException
         String jwt = tokenProvider.generateToken(user);
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt, Long.toString(user.getId()), user.getUsername(), user.getNameEtab(), user.isAdmin()));
 
     }
 }
-
 
 
--- web/src/test/java/fr/abes/licencesnationales/web/controllers/AuthenticationControllerTest.java
+++ web/src/test/java/fr/abes/licencesnationales/web/controllers/AuthenticationControllerTest.java
@@ -6,11 +6,10 @@ import fr.abes.licencesnationales.core.entities.EtablissementEntity;
 import fr.abes.licencesnationales.web.security.exception.DonneeIncoherenteBddException;
 import fr.abes.licencesnationales.web.security.payload.request.LoginRequest;
 import fr.abes.licencesnationales.web.security.services.impl.UserDetailsImpl;
-import fr.abes.licencesnationales.web.security.services.impl.UserDetailsServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,67 +18,80 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class AuthenticationControllerTest {
-    @Autowired
-    private WebApplicationContext context;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureMockMvc
+public class AuthenticationControllerTest
+{
 
+    /** pour faire marcher le test,
+     *  dans le MockUserUtil mettre "secret" au lieu de passwordEncoder.encode("OldPass1Test&")
+     */
+
+
+
+    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private PasswordEncoder passwordEncoder;
 
     @MockBean
-    private AuthenticationManager authenticationManager;
+    Authentication authentication;
 
     @MockBean
-    private UserDetailsServiceImpl service;
-
-    private EtablissementEntity user;
+    private AuthenticationManager authenticationManager;
 
     private static ObjectMapper mapper = new ObjectMapper();
 
 
     @BeforeEach
     public void init() throws DonneeIncoherenteBddException {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-        user = new MockUserUtil(passwordEncoder).getMockUser();
-        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("123456789", "secret"));
-        Mockito.when(service.loadUserByUsername(Mockito.anyString())).thenReturn(UserDetailsImpl.build(user));
+        EtablissementEntity localUser = new MockUserUtil(passwordEncoder).getMockUser();
+        Mockito.when(authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(localUser.getSiren(), localUser.getContact().getMotDePasse()))).thenReturn(authentication);
+
+        //Pour éviter la base de donnée :
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(localUser.getContact().getRole()));
+        UserDetailsImpl userDetails = new UserDetailsImpl(Long.valueOf(localUser.getId()),localUser.getSiren(), localUser.getName(), localUser.getContact().getMotDePasse(), localUser.getContact().getMail(), authorities,false);
+
+        //quand on fait :
+        Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+        //c'est comme si on faisait :
+        //Mockito.when(service.loadUserByUsername(localUser.getSiren())).thenReturn(UserDetailsImpl.build(localUser));
+
+        Mockito.when(passwordEncoder.matches(Mockito.anyString(), Mockito.anyString())).thenReturn(true);
     }
+
 
     @Test
     public void doLoginGet()  throws Exception {
         mockMvc.perform(get("/v1/login")).andExpect(status().isMethodNotAllowed());
     }
 
+
     @Test
     @DisplayName("test authentification réussie")
     public void testLoginSuccess() throws Exception {
         LoginRequest request = new LoginRequest();
-        request.setPassword("passwordtest");
+        request.setPassword("secret");
         request.setLogin("123456789");
-
-        UserDetailsImpl localUser = UserDetailsImpl.build(user);
-        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(localUser, null);
-        Mockito.when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
 
         String json = mapper.writeValueAsString(request);
         this.mockMvc.perform(post("/v1/login")
@@ -88,19 +100,48 @@ public class AuthenticationControllerTest {
                 .andExpect(jsonPath("$.accessToken").isNotEmpty());
     }
 
-
     @Test
     @DisplayName("test authentification mauvais login / mdp")
+    //derrière la NestedServletException il y a un NullPointerException ==> qd on charge le userDetailImpl
     public void testLoginFailed() throws Exception {
-        LoginRequest request = new LoginRequest();
-        request.setPassword("00000000");
-        request.setLogin("000000000");
+        assertThrows(NestedServletException.class,()->{ // pourquoi on obtient pas plutôt une BadCredentialsException?
+            LoginRequest request = new LoginRequest();
+            request.setPassword("secret");
+            request.setLogin("123654789");
 
-        Mockito.when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenThrow(new UsernameNotFoundException("000000000"));
+            this.mockMvc.perform(post("/v1/login")
+                    .contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(request)));
+        });
+    }
+
+    @Test
+    @DisplayName("test Validator Pattern marche")
+    //verif que @NotBlank et @Pattern du LoginRequest marche bien via @Valid RequestBody
+    public void testValidatorMarche() throws Exception {
+            LoginRequest request = new LoginRequest();
+            request.setPassword("");
+            request.setLogin("");
+
+            this.mockMvc.perform(post("/v1/login")
+                    .contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.message").value("The credentials are not valid"))
+                    .andExpect(jsonPath("$.debugMessage").value("Incorrect fields : Le SIREN doit contenir 9 chiffres, SIREN obligatoire (login), Mot de passe obligatoire (password), "));
+
+    }
+
+
+    @Test
+    @DisplayName("test Validator Pattern marche 2 ")
+    //cas siren 3 chiffres au lieu de 9 ==> on passe dans le ExceptionControllerHandler handleMethodArgumentNotValid package web
+    public void testValidatorTheCredentialsAreNotValid() throws Exception {
+        LoginRequest request = new LoginRequest();
+        request.setPassword("secret");
+        request.setLogin("123"); // ou "mmm" etc
 
         this.mockMvc.perform(post("/v1/login")
                 .contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("Credentials not valid"));
+                .andExpect(jsonPath("$.message").value("The credentials are not valid"));
     }
 }
