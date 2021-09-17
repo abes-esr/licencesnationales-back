90e3ac549deaf9ba1988fbdc393a607a4c2c164f
Théo Chambon <chambon@abes.fr>
2021-09-17 16:22:35 +0200

    Fix: Appel ReCAPTCHA en POST

--- web/src/main/java/fr/abes/licencesnationales/web/recaptcha/ReCaptchaKeys.java
+++ web/src/main/java/fr/abes/licencesnationales/web/recaptcha/ReCaptchaKeys.java
@@ -3,15 +3,20 @@ package fr.abes.licencesnationales.web.recaptcha;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter @Setter
-@ConfigurationProperties(prefix = "google.fr.abes.licencesnationales.web.recaptcha.key")
 public class ReCaptchaKeys {
 
+    @Value("${google.fr.abes.licencesnationales.recaptcha.key.site}")
     private String site;
+
+    @Value("${google.fr.abes.licencesnationales.recaptcha.key.secret}")
     private String secret;
+
+    @Value("${google.fr.abes.licencesnationales.recaptcha.key.threshold}")
     private float threshold;
 }
