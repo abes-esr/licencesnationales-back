0094d1074645ca030ea97582cab3ed2739363f2e
Fabien Rétif <retif@abes.fr>
2021-09-22 14:33:09 +0200

    Fix : ReCaptcha - mise à jour de la clé de l'action pour mot de passe oublié

--- web/src/main/java/fr/abes/licencesnationales/web/service/ReCaptchaAction.java
+++ web/src/main/java/fr/abes/licencesnationales/web/service/ReCaptchaAction.java
@@ -4,7 +4,7 @@ public abstract class ReCaptchaAction {
 
     public static final String CREATION_COMPTE = "creationCompte";
 
-    public static final String MOT_DE_PASSE_OUBLIE = "Passoublie";// TODO mettre la bonne valeur
+    public static final String MOT_DE_PASSE_OUBLIE = "forgotPassword";
 
     public static final String REINITIALISER_MOT_DE_PASSE = "reinitialisationPass";
 
