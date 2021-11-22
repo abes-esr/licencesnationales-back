f1b4c9155f1870644876e6a2bba3596c21c6739a
maraval <maraval@abes.fr>
2021-11-22 15:18:37 +0100

    Fix : ajout établissement / contact dans import.sql

--- web/src/main/resources/import.sql
+++ web/src/main/resources/import.sql
@@ -21,3 +21,10 @@ insert into type_etablissement(id, libelle) values(10, 'Fondation reconnues d ut
 insert into type_etablissement(id, libelle) values(11, 'GIP');
 insert into type_etablissement(id, libelle) values(12, 'Réseau Latitude France');
 insert into type_etablissement(id, libelle) values(13, 'Bibliothèques de lecture publique.');
+
+Insert into CONTACT (ID, ADRESSE,BOITE_POSTALE,CEDEX,CODE_POSTAL,MAIL,MOT_DE_PASSE,NOM,PRENOM,TELEPHONE,VILLE,ROLE) values (1, 'Adminee',null,null,'34000','tcn@abes.fr','$2a$10$8iU5R4Xw6GuBpH3U8jiO1.e.5DGbMsc68mOm86899NkAO.o7b0/Ki','Admin','Admin','0000000000','Admin','admin');
+Insert into CONTACT (ID, ADRESSE,BOITE_POSTALE,CEDEX,CODE_POSTAL,MAIL,MOT_DE_PASSE,NOM,PRENOM,TELEPHONE,VILLE,ROLE) values (2, 'Test Raluca',null,null,'34000','pierrot@abes.fr','$2a$10$Eq/IJHfRfuezZ6ESpacZOugUGvAUjm6thkE6GacaaA/DMHUJDsI4m','Test Raluca','Test Raluca','0633333333','Montpellier','admin');
+
+
+Insert into ETABLISSEMENT (ID,ID_ABES,DATE_CREATION,NOM,SIREN,CONTACT_ID,REF_STATUT,REF_TYPE_ETABLISSEMENT) values (1,'iCU9dGkJk4QycfJ71R98a51L',to_timestamp('07/10/21 14:34:44,033000000','DD/MM/RR HH24:MI:SSXFF'),'Admin','000000000',1,4,2);
+Insert into ETABLISSEMENT (ID,ID_ABES,DATE_CREATION,NOM,SIREN,CONTACT_ID,REF_STATUT,REF_TYPE_ETABLISSEMENT) values (2,'3gC9u31NH4Iwf72f2fgjf5pd',to_timestamp('02/11/21 16:35:15,911000000','DD/MM/RR HH24:MI:SSXFF'),'Test Raluca','123123123',2,4,2);
