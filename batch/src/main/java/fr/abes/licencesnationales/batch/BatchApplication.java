52f4cb8d4d5d321862aa7e924f556ca24fe2c485
maraval <maraval@abes.fr>
2021-11-26 08:43:59 +0100

    Feat : mise en place architecture batch pour exécuter 2 jobs séquentiellement

--- batch/src/main/java/fr/abes/licencesnationales/batch/BatchApplication.java
+++ batch/src/main/java/fr/abes/licencesnationales/batch/BatchApplication.java
@@ -1,9 +1,19 @@
 package fr.abes.licencesnationales.batch;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,9 +23,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("fr.abes.licencesnationales.core")
 @EnableJpaRepositories(basePackages = "fr.abes.licencesnationales.core.repository")
 public class BatchApplication {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
         long startTime = System.currentTimeMillis();
-        SpringApplication.run(BatchApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(BatchApplication.class, args);
+        JobLauncher jobLauncher = (JobLauncher) ctx.getBean("jobLauncher");
+        JobExecution executionJobGestionCompte = jobLauncher.run((Job) ctx.getBean("jobGestionCompte"), null);
+        if (ExitStatus.COMPLETED.equals(executionJobGestionCompte.getExitStatus())) {
+            jobLauncher.run((Job) ctx.getBean("jobRelances"), null);
+        }
+
         long endTime = System.currentTimeMillis();
         log.debug("Timing " + (endTime-startTime) +" ms");
         System.exit(0);
