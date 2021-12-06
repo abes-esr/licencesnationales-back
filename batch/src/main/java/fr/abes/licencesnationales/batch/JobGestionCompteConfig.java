cfe6fde541dbeea81aa1f6dfcc5d92cf31d0afe5
maraval <maraval@abes.fr>
2021-12-06 13:54:51 +0100

    Fix : ajout corrections sur la configuration des batch

--- batch/src/main/java/fr/abes/licencesnationales/batch/JobGestionCompteConfig.java
+++ batch/src/main/java/fr/abes/licencesnationales/batch/JobGestionCompteConfig.java
@@ -5,13 +5,15 @@ import fr.abes.licencesnationales.batch.gestionCompte.SuppressionEtEnvoiMailTask
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class JobGestionCompteConfig extends JobConfiguration {
     @Bean(name = "jobGestionCompte")
     public Job jobGestionCompte() {
-        return this.jobBuilderFactory.get("gestionCompte").incrementer(incrementer())
+        return this.jobBuilderFactory.get("gestionCompte").incrementer(new RunIdIncrementer())
                 .start(stepSelectEtablissement()).next(stepSuppressionCompte())
                 .build();
     }
