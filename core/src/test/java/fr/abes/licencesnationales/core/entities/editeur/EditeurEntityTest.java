a8065f6018488674c3185882ee83c2d2e4d54652
SamuelQuetin <samquetinuel@gmail.com>
2021-12-16 08:31:50 +0100

    Ajout: TU editeur etity

---
+++ core/src/test/java/fr/abes/licencesnationales/core/entities/editeur/EditeurEntityTest.java
@@ -0,0 +1,40 @@
+package fr.abes.licencesnationales.core.entities.editeur;
+
+import fr.abes.licencesnationales.core.entities.TypeEtablissementEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.HashSet;
+import java.util.Set;
+
+class EditeurEntityTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void init() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    @DisplayName("test validation creation editeur Null ")
+    void testValidateCreatEtablissemenNull() {
+        TypeEtablissementEntity type = new TypeEtablissementEntity(1, "Nouveau");
+        Set<TypeEtablissementEntity> setType = new HashSet<>();
+        setType.add(type);
+
+        EditeurEntity editeur = new EditeurEntity(1,"","","",setType);
+
+        Set<ConstraintViolation<EditeurEntity>> violationsEditeur = validator.validate(editeur);
+        Assertions.assertEquals(2,violationsEditeur.size());
+        Assertions.assertEquals("Le nom de l'éditeur fourni n'est pas valide", violationsEditeur.stream().filter(v -> v.getPropertyPath().toString().equals("nom")).findFirst().get().getMessage());
+        Assertions.assertEquals("L'adresse postale fournie n'est pas valide", violationsEditeur.stream().filter(v -> v.getPropertyPath().toString().equals("adresse")).findFirst().get().getMessage());
+    }
+}
