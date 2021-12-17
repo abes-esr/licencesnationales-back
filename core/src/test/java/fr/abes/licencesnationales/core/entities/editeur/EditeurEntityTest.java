a58696992a843b9ad8f9a159b2ccb16af781d5a5
SamuelQuetin <samquetinuel@gmail.com>
2021-12-17 11:50:38 +0100

    ajout : TU sur validator editeur null et blank

--- core/src/test/java/fr/abes/licencesnationales/core/entities/editeur/EditeurEntityTest.java
+++ core/src/test/java/fr/abes/licencesnationales/core/entities/editeur/EditeurEntityTest.java
@@ -24,13 +24,28 @@ class EditeurEntityTest {
     }
 
     @Test
-    @DisplayName("test validation creation editeur Null ")
-    void testValidateCreatEtablissemenNull() {
+    @DisplayName("test validation creation editeur Blank ")
+    void testValidateCreatEditeurBlank() {
         TypeEtablissementEntity type = new TypeEtablissementEntity(1, "Nouveau");
         Set<TypeEtablissementEntity> setType = new HashSet<>();
         setType.add(type);
 
         EditeurEntity editeur = new EditeurEntity(1,"","","",setType);
+
+        Set<ConstraintViolation<EditeurEntity>> violationsEditeur = validator.validate(editeur);
+        Assertions.assertEquals(2,violationsEditeur.size());
+        Assertions.assertEquals("Le nom de l'éditeur fourni n'est pas valide", violationsEditeur.stream().filter(v -> v.getPropertyPath().toString().equals("nom")).findFirst().get().getMessage());
+        Assertions.assertEquals("L'adresse postale fournie n'est pas valide", violationsEditeur.stream().filter(v -> v.getPropertyPath().toString().equals("adresse")).findFirst().get().getMessage());
+    }
+
+    @Test
+    @DisplayName("test validation creation editeur Null ")
+    void testValidateCreatEditeurNull() {
+        TypeEtablissementEntity type = new TypeEtablissementEntity(1, "Nouveau");
+        Set<TypeEtablissementEntity> setType = new HashSet<>();
+        setType.add(type);
+
+        EditeurEntity editeur = new EditeurEntity(1,null,null,null,setType);
 
         Set<ConstraintViolation<EditeurEntity>> violationsEditeur = validator.validate(editeur);
         Assertions.assertEquals(2,violationsEditeur.size());
