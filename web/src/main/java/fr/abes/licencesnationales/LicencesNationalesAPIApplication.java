88120fb75ece107ab80abcef431faa6ff140f697
Stéphane Gully <stephane.gully@gmail.com>
2022-03-15 19:03:12 +0100

    fix: config du cors pour les différentes URL de dev/test

--- web/src/main/java/fr/abes/licencesnationales/LicencesNationalesAPIApplication.java
+++ web/src/main/java/fr/abes/licencesnationales/LicencesNationalesAPIApplication.java
@@ -38,7 +38,22 @@ public class LicencesNationalesAPIApplication extends SpringBootServletInitializ
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // *** URL below needs to match the Vue client URL and port ***
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+        
+        // Config for allowing CORS for the local, dev, test, prod internal servers
+        // not necessary to set anything for production URL because de interne domain/port
+        // is the same for licencesnationales-back and licencesnationales-front:
+        // https://acces.licencesnationales.fr/ => for the front
+        // https://acces.licencesnationales.fr/v1/ => for the back (api)
+        List<String> allowedOriginsUrl = new ArrayList<>();
+        allowedOriginsUrl.add("http://localhost:8080");
+        allowedOriginsUrl.add("http://localhost:8081");
+        allowedOriginsUrl.add("http://127.0.0.1:8081");
+        allowedOriginsUrl.add("http://127.0.0.1:6081");
+        allowedOriginsUrl.add("http://diplotaxis-test.v202.abes.fr:6081");
+        allowedOriginsUrl.add("http://diplotaxis-dev.v212.abes.fr:6081");
+        allowedOriginsUrl.add("http://diplotaxis-prod.v212.abes.fr:6081");
+        config.setAllowedOrigins(allowedOriginsUrl);
+        
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
